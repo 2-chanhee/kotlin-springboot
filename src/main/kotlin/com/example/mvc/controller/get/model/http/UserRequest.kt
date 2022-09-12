27e632d15a76fb4ed81f736340f8bfe27df89832
2-chanhee <l01083582454@gmail.com>
2022-09-12 12:14:10 +0900

    POST

--- src/main/kotlin/com/example/mvc/controller/get/model/http/UserRequest.kt
+++ src/main/kotlin/com/example/mvc/controller/get/model/http/UserRequest.kt
@@ -1,8 +1,17 @@
 package com.example.mvc.controller.get.model.http
 
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
+import com.fasterxml.jackson.databind.annotation.JsonNaming
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy::class) // 해당 클래스의 프로퍼티는 snake_case로 동작한다는 것을 명시
 data class UserRequest(
     var name: String?=null,
     var age: Int?=null,
     var email: String?=null,
-    var address: String?=null
+    var address: String?=null,
+    var phoneNumber: String?=null // phone_number
+    
+//    @JsonProperty("phone_number") // 요청 시 이 프로퍼티명으로 할것이라는 것을 명시
+//    var phoneNumber: String?=null // phone_number
 )
